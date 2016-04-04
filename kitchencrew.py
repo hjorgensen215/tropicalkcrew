3c82cb25c05bacc2b622cc9ed7a00c52730dbc52
maliha hollis <malihahollis@malihas-mbp.cate.org>
2016-04-04 11:00:27 -0700

    first draft of crew rotation

---
+++ kitchencrew.py
@@ -0,0 +1,13 @@
+names= [mike, bob, joel, hannah, cooper, cool, hip]
+offday_list=[]
+week= 'N, S'
+week= raw_input('Is it an N or S week?')
+if week== 'N':
+    for i in range(8):
+        names.pop(i-1 and i-2)
+        offday_list.append(i-1 and i-2)
+if week== 'S':
+    for i in range(11):
+        names.pop(i-1 and i-2)
+        offday_list.append(i-1 and i-2)
+print week 
