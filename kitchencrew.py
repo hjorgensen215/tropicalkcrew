5c8dc6512c2ae44ea61cff925d82540ff5714c21
maliha hollis <malihahollis@malihas-mbp.cate.org>
2016-04-04 11:02:01 -0700

    added quotes to names list

--- kitchencrew.py
+++ kitchencrew.py
@@ -1,4 +1,4 @@
-names= [mike, bob, joel, hannah, cooper, cool, hip]
+names= ['mike', 'bob', 'joel', 'hannah', 'cooper', 'cool', 'hip']
 offday_list=[]
 week= 'N, S'
 week= raw_input('Is it an N or S week?')
@@ -10,4 +10,4 @@ if week== 'S':
     for i in range(11):
         names.pop(i-1 and i-2)
         offday_list.append(i-1 and i-2)
-print week 
+print week
