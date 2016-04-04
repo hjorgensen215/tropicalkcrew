cbd9ce821416f66e62ddf71bdedd4e7e7b473098
maliha hollis <malihahollis@malihas-mbp.cate.org>
2016-04-04 11:15:45 -0700

    changed the ranges we pop

--- kitchencrew.py
+++ kitchencrew.py
@@ -4,10 +4,10 @@ week= 'N, S'
 week= raw_input('Is it an N or S week?')
 if week== 'N':
     for i in range(8):
-        names.pop(i-1 and i-2)
-        offday_list.append(i-1 and i-2)
+        names.pop[-2:]
+        offday_list.append[-2:]
 if week== 'S':
     for i in range(11):
-        names.pop(i-1 and i-2)
-        offday_list.append(i-1 and i-2)
+        names.pop[-2:]
+        offday_list.append[-2:]
 print week
