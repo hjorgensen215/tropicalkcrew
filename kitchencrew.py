a9862651aa11aaf938a3de71922ad2f88f7bc7c0
maliha hollis <malihahollis@malihas-mbp.cate.org>
2016-04-05 11:47:56 -0700

    changed brakets to parenthesis again

--- kitchencrew.py
+++ kitchencrew.py
@@ -4,11 +4,11 @@ week= 'N, S'
 week= raw_input('Is it an N or S week?')
 if week== 'N':
     for i in range(8):
-        myvar= names.pop[1]
+        myvar= names.pop(1)
         offday_list.append(myvar)
 elif week== 'S':
     for i in range(11):
-        myvars= names.pop[1]
+        myvars= names.pop(1)
         offday_list.append(myvars)
 print week
 print offday_list
