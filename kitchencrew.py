b5c41ba0e806052e426cf2cbc3d183939ab49e00
maliha hollis <malihahollis@malihas-mbp.cate.org>
2016-04-05 11:48:35 -0700

    changed range

--- kitchencrew.py
+++ kitchencrew.py
@@ -3,11 +3,11 @@ offday_list=[]
 week= 'N, S'
 week= raw_input('Is it an N or S week?')
 if week== 'N':
-    for i in range(8):
+    for i in range(4):
         myvar= names.pop(1)
         offday_list.append(myvar)
 elif week== 'S':
-    for i in range(11):
+    for i in range(4):
         myvars= names.pop(1)
         offday_list.append(myvars)
 print week
